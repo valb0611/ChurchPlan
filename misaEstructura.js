4564595ee074a353aeedfc3d4e50e899e0897a2b
valb0611 <2016467@est.cedesdonbosco.ed.cr>
2024-03-26 08:23:35 -0600

    subo misa.html

--- misaEstructura.js
+++ misaEstructura.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mongoose =require('mongoose');
 
 module.exports = mongoose.model('misa', {
@@ -30,37 +29,4 @@ module.exports = mongoose.model('misa', {
     hora : String,
 
     
-=======
-var mongoose =require('mongoose');
-
-module.exports = mongoose.model('misa', {
-
-    organizador : String,
-    lectura1 : String,
-    lectura2 : String,
-    salmo : String,
-    evangelio : String,
-    miembrosCoro1 : String,
-    miembrosCoro2 : String,
-    miembrosCoro3 : String,
-    miembrosCoro4 : String,
-    miembrosCoro5 : String,
-    cancion1 : String,
-    cancion2 : String,
-    cancion3 : String,
-    cancion4 : String,
-    cancion5 : String,
-    cancion6 : String,
-    cancion7 : String,
-    cancion8 : String,
-    cancion9 : String,
-    monaguillo1 : String,
-    monaguillo2 : String,
-    monaguillo3 : String,
-    monaguillo4 : String,
-    fecha : String,
-    hora : String,
-
-    
->>>>>>> 29ea02d334301b56c1cebbc581882dd7205a176f
 });
--- usuarioEstructura.js
+++ usuarioEstructura.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mongoose =require('mongoose');
 
 module.exports = mongoose.model('usuario', {
@@ -10,17 +9,4 @@ module.exports = mongoose.model('usuario', {
     
     
 
-=======
-var mongoose =require('mongoose');
-
-module.exports = mongoose.model('usuario', {
-
-    
-    correo : String,
-    clave : String,
-    nombreCompleto : String,
-    
-    
-
->>>>>>> 29ea02d334301b56c1cebbc581882dd7205a176f
 });
